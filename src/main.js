f8be2422e8a44db7e817a13a5b2853344de28d17
NoavanDijk <47594974+NoavanDijk@users.noreply.github.com>
2020-04-29 14:33:59 +0200

    Vuelidate installed

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import { store }  from './store/store';
 import { routes } from './routes';
 import VueCarousel from 'vue-carousel';
+import Vuelidate from 'vuelidate';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 Vue.use(VueCarousel);
+Vue.use(Vuelidate);
 
 const router = new VueRouter({
   routes
