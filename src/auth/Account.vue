71f0c7ce3b9a0405bdc31dab4b213b09dc3847fa
NoavanDijk <47594974+NoavanDijk@users.noreply.github.com>
2020-06-28 19:38:05 +0200

    Small adds and deletes

--- src/auth/Account.vue
+++ src/auth/Account.vue
@@ -10,8 +10,6 @@
     <div v-for="(totalPrice) in totalPrices" :key="totalPrice">
       <p>{{ totalPrice }}</p>
     </div>
-
-    <!-- Laat de bestellingen van de ingelogde gebruiker zien -->
   </div>
 </template>
 
@@ -19,6 +17,7 @@
 import { mapGetters } from "vuex";
 import firebase from "firebase";
 import axios from "axios";
+import moment from 'moment';
 
 export default {
   data() {
@@ -35,18 +34,11 @@ export default {
 
   computed: {
     ...mapGetters({
-      // map `this.user` to `this.$store.getters.user`
       user: "user",
     }),
   },
 
   created() {
-    // get alle items van de database op
-    // currentUser is
-    // for(alle items in de database id's){
-    //   if (currentUser == opgeslagen id) {
-    // }
-    // }
     this.createdMethod();
   },
 
@@ -61,45 +53,20 @@ export default {
           for (let key in this.results) {
             this.results[key].id = key;
             choices.push(this.results[key]);
-            // console.log(choices);
           }
 
           this.finalChoices = choices;
-          // console.log(this.finalChoices);
-
           
           for(var i = 0; i < this.finalChoices.length; i++) {
             if(this.finalChoices[i][0] == firebase.auth().currentUser.uid) {
               this.sameIDs.push(this.finalChoices[i]);
-              console.log(this.sameIDs);
-              // console.log("komt overeen");
             }
           }
 
           for(var j = 0; j < this.sameIDs.length; j++) {
-            this.dates.push(this.sameIDs[j][1]);
+            this.dates.push(moment.unix(this.sameIDs[j][1]).format("MM/DD/YYYY"));
             this.totalPrices.push(this.sameIDs[j][2]);
-
-            // Laat de prijs en de datum zien
           }
-
-          var userIDs = [];
-          // for (var i = 0; i < this.finalChoices.length; i++) {
-          //   userIDs.push(this.finalChoices[i][0]);
-            
-          //   this.allUsersIDs = userIDs;
-          //   console.log(this.allUsersIDs);
-
-          //   for (let j = 0; j < this.allUsersIDs.length; j++) {
-          //     // console.log(this.allUsersIDs[i]);
-          //     // console.log(firebase.auth().currentUser.uid);
-          //     if (this.allUsersIDs[i] == firebase.auth().currentUser.uid) {
-                
-          //       this.currentUserIDs = this.allUsersIDs[i];
-          //       // console.log(this.currentUserIDs);
-          //     }
-          //   }
-          // }
         })
         .catch((error) => console.log(error));
     },
